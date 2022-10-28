c1185085e715d64270a911e1ef110ef53c478b0c
Textiny <96368206+Textiny@users.noreply.github.com>
2022-10-28 14:00:13 +0300

    Create test.cs

---
+++ test.cs
@@ -0,0 +1,23 @@
+int x = 1500; //number
+
+switch (x)
+{
+  case 10:
+    Console.WriteLine("x = 10");
+    break;
+  case 50:
+    Console.WriteLine("x = 50");
+    break;
+  case 500:
+    Console.WriteLine("x = 500");
+    break;
+  case 1500:
+    Console.WriteLine("x = 1500");
+    break;
+  case 5000:
+    Console.WriteLine("x = 5000");
+    break;
+  default:
+    Console.WriteLine("----------------")
+    break;
+}
